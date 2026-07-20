6346112af4bfa2c0319510db72ff8cdfda5ce0f0
Toluwaloju Kayode <tolukayode2017@gmail.com>
2026-07-20 12:19:00 +0100

    added host settings to the main file

--- backend/v1/main.py
+++ backend/v1/main.py
@@ -78,4 +78,4 @@ app.include_router(messages)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=settings.backend_port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=settings.backend_port, reload=True)
